e83065f123bab36e0b082a24cefdbbe159907731
yasminadaileh1 <yas.adaileh1@gmail.com>
2020-02-21 13:36:45 +0200

    solving q2 problem

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
 var sumA = a + b
-return [sumA , `The sum of ${a} and ${b} is ${sumA}.`]
+return [sumA , `The sum of ${a} and ${b} is ${sumA}.`];
 }
 
 // Here is the test for sum(); uncomment it to run it
@@ -28,11 +28,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+var multiplyA = a * b
+return [ multiplyA , `The product of ${a} and ${b} is ${multiplyA}.`];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
